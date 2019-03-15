44627055c8c6e4a6150e6cf2233eaa8994255617
Daniel Balcer <kemaron@gmail.com>
2019-03-15 19:16:16 +0100

    Add example for This&Self

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/* ----- tworzenie klasy i prototypowanie ----- */
 function Phone(brand, price, color, screen, battery) {
 	this.brand = brand;
 	this.price = price;
@@ -19,5 +20,31 @@ Phone.prototype.printDetails = function() {
 
 var AquarisX2 = new Phone ("BQ",1350,"black","5.65''",3100);
 
+// test Czy dziala
 AquarisX2.printInfo();
 AquarisX2.printDetails();
+
+/* ----- wykorzystanie this i self ----- */
+
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.element = document.createElement('button');
+		this.element.innerText = this.text;
+		this.element.addEventListener('click', function() {
+			alert(self.text);
+		});
+		document.body.appendChild(this.element);
+	}
+}
+
+// test Czy dziala
+var btn1 = new Button('Check this buttton');
+btn1.create();
+
+var btn2 = new Button();
+btn2.create();
